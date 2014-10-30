e59b9461051e507988cce4106cc167dc77d29230
Trokan <mail@simonpontius.de>
2014-10-30 11:20:37 +0100

    Add ImageEditorFrame

---
+++ ImageEditorFrame.java
@@ -0,0 +1,22 @@
+import javax.swing.*; 
+import java.awt.event.*; 
+import java.awt.*; 
+import java.awt.image.*; 
+import java.io.*; import 
+javax.imageio.*;
+
+public class ImageEditorFrame extends JFrame{
+
+	
+	public static void main(String[] args) {
+		ImageEditorFrame frame = new ImageEditorFrame();
+
+	}
+	
+	public ImageEditorFrame(){ 
+		setDefaultCloseOperation(EXIT_ON_CLOSE); 
+		setSize(400, 300); 
+		setVisible(true); 
+	}
+
+}
